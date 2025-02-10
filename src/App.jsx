4c771dba7222bb3f029452b4a51d8b3f10784416
Agustin <agustinruiz3007@gmail.com>
2025-02-10 10:29:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Home } from "./components/Home";
+
+function App() {
+	return (
+		<main className="min-h-svh bg-gradient-to-tr from-base to-primary">
+			<Home />
+		</main>
+	);
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import { faSpotify } from "@fortawesome/free-brands-svg-icons/faSpotify";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const NavBar = () => {
+	return (
+		<nav className="flex justify-between items-center p-2 lg:px-10 w-full max-w-screen-[1920px] mx-auto text-white">
+			<div className="flex items-center gap-4 md:gap-6">
+				<a href="#">
+					<FontAwesomeIcon icon={faSpotify} size="2xl" width={28} />
+				</a>
+				<ul className="flex gap-4 md:gap-6">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">TV Shows</a>
+					</li>
+					<li>
+						<a href="#">Movies</a>
+					</li>
+				</ul>
+			</div>
+			<div className="flex items-center gap-2">
+				<button className="cursor-pointer">
+					<FontAwesomeIcon icon={faSearch} size="2xl" width={22} />
+				</button>
+				<button className="text-2xl w-8 aspect-square lg:hidden cursor-pointer">
+					<FontAwesomeIcon icon={faBars} />
+				</button>
+			</div>
+		</nav>
+	);
+};
